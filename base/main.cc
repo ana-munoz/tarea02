5e48b0861fe552f7357b0a251b68461cc65b4186
ana-munoz <ana.munoz@alumnos.uv.cl>
2022-05-05 21:55:32 -0500

    hace la multiplicación para ambos métodos, por comprobar si está bien jaja

--- base/main.cc
+++ base/main.cc
@@ -29,8 +29,11 @@ int main(int argc, char** argv)
 {
 
 	Timer<std::chrono::nanoseconds> timer1;
+	Timer<std::chrono::nanoseconds> timer2;
+	Timer<std::chrono::nanoseconds> timer3;
+
 	std::string fileMatrixA;
-	
+
 	///////////////////////////////////////
 	//  Read command-line parameters
 	if(argc != 3){
@@ -44,14 +47,14 @@ int main(int argc, char** argv)
 			fileMatrixA = argv[i+1];
 		}
 	}
-	
+
 	if(fileMatrixA == ""){
 		uso(argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	Matrix<float> m1(fileMatrixA, " ");
-	
+
 	timer1.start();
 	try{
 		m1.load();
@@ -60,25 +63,54 @@ int main(int argc, char** argv)
 		exit(EXIT_FAILURE);
 	}
 	timer1.stop();
-	
 	std::cout << "Time to load matrix in memory: " << timer1.elapsed() << " ns\n";
-	
-	for(size_t i=0; i< m1.rows(); i++){
+
+/*	for(size_t i=0; i< m1.rows(); i++){
 		for(size_t j=0; j< m1.cols(); j++){
 			std::cout << m1(i,j) << "\t";
 		}
 		std::cout << std::endl;
 	}
-	
-	
+*/
+
 	// Ejemplo de creación de matrix vacía
-/*	MultMatrix mm;
+	MultMatrix mm;
+	timer2.start();
 	Matrix<float> C(m1.rows(), m1.cols());
-	
+
 	//Ejemplo de llamada al método del algoritmo ijk
 	mm.DOijk(m1, m1, C);
-*/	
-	
+	timer2.stop();
+
+        std::cout << "Timer DOijk: " << timer2.elapsed() << " ns\n";
+	std::cout << "DOIJK" << "\t";
+	for(size_t i=0; i<C.rows(); i++){
+		for(size_t j=0;j<C.cols();j++){
+			std::cout << C(i,j) << "\t";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << "DOKIJ";
+	std::cout << std::endl;
+
+	MultMatrix mm2;
+	timer3.start();
+	Matrix<float> C_prima(m1.rows(), m1.cols()); 
+
+	mm2.DOkij(m1, m1, C_prima);
+	timer3.stop();
+
+        std::cout << "Timer DOkij: " << timer3.elapsed() << " ns\n";
+
+	for(size_t i=0; i < C_prima.rows(); i++){
+		for(size_t j=0; j < C_prima.cols(); j++){
+			std::cout << C_prima(i,j) << "\t";
+		}
+		std::cout << std::endl;
+	}
+
+
 	return(EXIT_SUCCESS);
 }
 
